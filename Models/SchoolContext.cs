f0bab494fd24411b8e2e1442614cc86d6b56b7e6
RENE ALFREDO MURILLO RIVERA <smis273413@ugb.edu.sv>
2019-08-20 22:23:15 +0100

    Refactoring context

--- Models/SchoolContext.cs
+++ Models/SchoolContext.cs
@@ -30,18 +30,63 @@ namespace platziNetCore.Models
             school.Country = "Colombia";
             school.SchoolType = SchoolType.HighSchool;
 
+            //load School Courses
+            var courses = LoadCourses(school);
+
+            //For each course load subjects
+            var subjects = LoadSubjects(courses);
+
+            //For each course load students
+            var students = LoadStudents(courses);
+
             modelBuilder.Entity<School>().HasData(school);
-            modelBuilder.Entity<Subject>().HasData(
-                    new Subject { Name = "Mathematics", Id = Guid.NewGuid().ToString()},
-                    new Subject { Name = "Sports", Id = Guid.NewGuid().ToString()},
-                    new Subject { Name = "Spanish", Id = Guid.NewGuid().ToString()},
-                    new Subject { Name = "Science", Id = Guid.NewGuid().ToString()},
-                    new Subject { Name = "Programming", Id = Guid.NewGuid().ToString()}
-            );
-            modelBuilder.Entity<Student>().HasData(GenerateStudentsRandom().ToArray());
+            modelBuilder.Entity<Course>().HasData(courses.ToArray());
+            modelBuilder.Entity<Subject>().HasData(subjects.ToArray());
+            modelBuilder.Entity<Student>().HasData(students.ToArray());
         }
 
-        private List<Student> GenerateStudentsRandom()
+        private static List<Subject> LoadSubjects(List<Course> courses)
+        {
+            var fullList = new List<Subject>();
+            foreach (var course in courses)
+            {
+                var tmpSubjectList = new List<Subject> {
+                    new Subject { Id = Guid.NewGuid().ToString(), CourseId = course.Id, Name = "Mathematics" },
+                    new Subject { Id = Guid.NewGuid().ToString(), CourseId = course.Id, Name = "Sports" },
+                    new Subject { Id = Guid.NewGuid().ToString(), CourseId = course.Id, Name = "Spanish" },
+                    new Subject { Id = Guid.NewGuid().ToString(), CourseId = course.Id, Name = "Science" },
+                    new Subject { Id = Guid.NewGuid().ToString(), CourseId = course.Id, Name = "Programming" }
+                };
+                fullList.AddRange(tmpSubjectList);
+            }
+
+            return fullList;
+        }
+
+        private static List<Course> LoadCourses(School school)
+        {
+            return new List<Course>(){
+                new Course() {Id = Guid.NewGuid().ToString(), SchoolId = school.Id, Name = "101", Journey = JourneyType.Morning},
+                new Course() {Id = Guid.NewGuid().ToString(), SchoolId = school.Id, Name = "201", Journey = JourneyType.Morning},
+                new Course() {Id = Guid.NewGuid().ToString(), SchoolId = school.Id, Name = "301", Journey = JourneyType.Afternoon},
+                new Course() {Id = Guid.NewGuid().ToString(), SchoolId = school.Id, Name = "401", Journey = JourneyType.Afternoon},
+                new Course() {Id = Guid.NewGuid().ToString(), SchoolId = school.Id, Name = "501", Journey = JourneyType.Night},
+            };
+        }
+
+        private List<Student> LoadStudents(List<Course> courses)
+        {
+            var studentList = new List<Student>();
+            Random rnd = new Random();
+            foreach (var course in courses)
+            {
+                int quantRandom = rnd.Next(5,20);
+                var tmpList = GenerateStudentsRandom(course, quantRandom);
+                studentList.AddRange(tmpList);
+            }
+            return studentList;
+        }
+        private List<Student> GenerateStudentsRandom(Course course, int quantity)
         {
             string[] name1 = {"Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolas"};
             string[] name2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes"};
@@ -50,9 +95,11 @@ namespace platziNetCore.Models
             var studentsList = from n1 in name1 
                                 from n2 in name2
                                 from s1 in surname
-                                select new Student {Name = $"{n1} {n2} {s1}", Id = Guid.NewGuid().ToString() };
+                                select new Student {Name = $"{n1} {n2} {s1}", 
+                                                    CourseId = course.Id,
+                                                    Id = Guid.NewGuid().ToString() };
 
-            return studentsList.OrderBy( al => al.Id).ToList();
+            return studentsList.OrderBy( al => al.Id).Take(quantity).ToList();
         }
     }
 }
